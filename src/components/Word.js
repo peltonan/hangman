ea356d35f0d6c0a36870dbb86060ad52424111a3
peltonan <peltonen.annette@gmail.com>
2022-05-19 16:04:04 +0300

    Code-along done, and fixed typos

--- src/components/Word.js
+++ src/components/Word.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React from 'react';
 
-function Word({ selectedWord, correctLetters }) {
+const Word = ({ selectedWord, correctLetters }) => {
+
     return (
         <div className="word">
-            {selectedWord.split('').map((letter, index) => {
+            {selectedWord.split('').map((letter, i) => {
                 return (
-                    <span className="letter" key={index}>
+                    <span className="letter" key={i}>
                         {correctLetters.includes(letter) ? letter : ''}
                     </span>
                 )
